baee3c6cacf9f97b70601bc28380c15062afcaac
PalakTolwani <tolwanipalak76@gmail.com>
2025-09-09 12:19:07 +0530

    Data Validation completed

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+# ROOT_DIR ko src se ek level upar le jao
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent  
+
+CONFIG_FILE_PATH = ROOT_DIR / "config" / "config.yaml"
+PARAMS_FILE_PATH = ROOT_DIR / "params.yaml"
